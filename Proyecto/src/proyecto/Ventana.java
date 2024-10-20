308fce6467f8208de1560ff0ef6900a7559a2fc1
nicole0807-glitch <nicole.tolve@correo.unimet.edu.ve>
2024-10-20 15:56:07 -0400

    Ventana update

--- Proyecto/src/proyecto/Ventana.java
+++ Proyecto/src/proyecto/Ventana.java
@@ -4,8 +4,8 @@ import org.graphstream.ui.view.Viewer;
 //import org.graphstream.ui.swingViewer.ViewPanel;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
-//import proyecto.Menu.java;
 
 
 
@@ -13,24 +13,24 @@ public class Ventana extends javax.swing.JFrame {
 
     public Ventana() {
         initComponents();
-        setLocationRelativeTo(null);
+        //setLocationRelativeTo(null);
         setLocation(400,150);
-        setSize(500,500);
+        setSize(700,500);
         setResizable(false);
         //this.setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Cobertura de Sucursales");
+     
+        
+         
     }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         cargararchivos = new javax.swing.JButton();
-        EXIT = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -41,35 +41,11 @@ public class Ventana extends javax.swing.JFrame {
                 cargararchivosActionPerformed(evt);
             }
         });
-        getContentPane().add(cargararchivos, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 250, 140, 30));
+        getContentPane().add(cargararchivos, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 230, 170, 30));
 
-        EXIT.setText("X");
-        EXIT.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                EXITActionPerformed(evt);
-            }
-        });
-        getContentPane().add(EXIT, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 30, 50, -1));
-
-        jLabel2.setFont(new java.awt.Font("Arial Black", 0, 36)); // NOI18N
-        jLabel2.setText("BIENVENIDOS ");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 110, -1, 40));
-
-        jLabel3.setFont(new java.awt.Font("Arial Black", 0, 36)); // NOI18N
-        jLabel3.setText("     A");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 150, -1, -1));
-
-        jLabel4.setFont(new java.awt.Font("Arial Black", 0, 24)); // NOI18N
-        jLabel4.setText("COBERTURA DE SUCURSALES");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 200, -1, -1));
-
-        jButton1.setText("Otra ventana");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 300, -1, -1));
+        jLabel4.setFont(new java.awt.Font("Microsoft PhagsPa", 1, 36)); // NOI18N
+        jLabel4.setText(" COBERTURA DE SUCURSALES");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 140, 510, 80));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -86,18 +62,10 @@ public class Ventana extends javax.swing.JFrame {
         else {
             JOptionPane.showMessageDialog(null,"Archivo no seleccionado");
         }
-        
+        Menu v2 = new Menu();
+        v2.setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_cargararchivosActionPerformed
-
-    private void EXITActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EXITActionPerformed
-       this.dispose();
-    }//GEN-LAST:event_EXITActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        this.dispose();
-       // JFrame ventana2 = new Jframe();
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -137,11 +105,7 @@ public class Ventana extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton EXIT;
     private javax.swing.JButton cargararchivos;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     // End of variables declaration//GEN-END:variables
 }
