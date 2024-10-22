e4d536e21907c300651fe530de4484b5eada7d38
veronicagironvl <veronica.giron@correo.unimet.edu.ve>
2024-10-22 12:47:24 -0400

    actualizacion lista

---
+++ src/Lista/Parada.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lista;
+
+/**
+ *
+ * @author veron
+ */
+public class Parada {
+    private String nombre;
+    private int idParada; // Numero identificador asignado para identificar a la parada
+    private ListaEnlazada conexiones; // Lista de conexiones directas a otras paradas
+    private boolean esSucursal;
+    
+
+    public Parada(String nombre) {
+        this.nombre = nombre;
+        this.esSucursal = false;
+        this.conexiones = new ListaEnlazada();
+        this.idParada = -1;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setIDParada(int numero){
+        this.idParada = numero;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void agregarConexion(Parada parada){
+        
+    }
+    
+    public boolean esSucursal(){
+        return esSucursal;
+    }
+    
+    public void setEsSucursal(boolean esSucursal){
+        this.esSucursal = esSucursal;
+    }
+    
+}
